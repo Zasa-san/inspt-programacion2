c90761ad7fddaac2dd3c89da07a2284139e253bb
Bruno Schneider <bschneider@navent.com>
2025-06-27 12:23:44 -0300

    Sumo printer para las bases y toString para algunas clases

---
+++ src/bdd/printer/DBPrinter.java
@@ -0,0 +1,15 @@
+package bdd.printer;
+
+import java.util.List;
+
+public class DBPrinter {
+
+    private DBPrinter(){}
+
+    public static <T> void print(String name, List<T> data) {
+        System.out.println("Visualizando registros en " + name + "...\n");
+        System.out.println(name + "\n{");
+        data.forEach(item -> System.out.println(item.toString() + ","));
+        System.out.println("}\n");
+    }
+}
