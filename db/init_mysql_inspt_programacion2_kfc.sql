3fc4727e46d012e7f12b0c35ecb963d638788865
Santiago Borrajo <borrajosantiago@gmail.com>
2025-12-06 12:04:57 -0300

    dejar que hibernate cree todo

--- db/init_mysql_inspt_programacion2_kfc.sql
+++ db/init_mysql_inspt_programacion2_kfc.sql
@@ -4,14 +4,3 @@ CREATE DATABASE IF NOT EXISTS `inspt_programacion2_kfc`
   COLLATE utf8mb4_unicode_ci;
 
 USE `inspt_programacion2_kfc`;
-
--- Tabla de usuarios
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `enabled` TINYINT(1) NOT NULL DEFAULT 1,
-  `role` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_users_username` (`username`)
-);
